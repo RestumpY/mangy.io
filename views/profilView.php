5158c5ec42358ca6e53e3fcde6145d3127f83c05
RestumpY <70935893+RestumpY@users.noreply.github.com>
2021-11-25 15:17:19 +0100

    Interface utilisateur

---
+++ views/profilView.php
@@ -0,0 +1,105 @@
+<?php ob_start(); ?>
+<br>
+                <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <!-- Row -->
+                <div class="row">
+                    <!-- Column -->
+                    <div class="col-lg-4 col-xlg-3 col-md-12">
+                        <div class="white-box">
+                            <div class="user-bg"> <img width="100%" alt="user" src="plugins/images/large/img1.jpg">
+                                <div class="overlay-box">
+                                    <div class="user-content">
+                                        <a href="javascript:void(0)"><img src="plugins/images/users/genu.jpg"
+                                                class="thumb-lg img-circle" alt="img"></a>
+                                        <h4 class="text-white mt-2"><?php echo $user[0]['first_name'] . " " .$user[0]['last_name'];?></h4>
+                                        <h5 class="text-white mt-2"><?php echo $user[0]['email'];?>"</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--<div class="user-btm-box mt-5 d-md-flex">
+                                <div class="col-md-4 col-sm-4 text-center">
+                                    <h1>258</h1>
+                                </div>
+                                <div class="col-md-4 col-sm-4 text-center">
+                                    <h1>125</h1>
+                                </div>
+                                <div class="col-md-4 col-sm-4 text-center">
+                                    <h1>556</h1>
+                                </div>
+                            </div>-->
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-lg-8 col-xlg-9 col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <form class="form-horizontal form-material">
+                                    <div class="form-group mb-4">
+                                        <label class="col-md-12 p-0">Full Name</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="text" placeholder="<?php echo $user[0]['first_name'] . " ". $user[0]['last_name'];?>"
+                                                class="form-control p-0 border-0"> </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label for="example-email" class="col-md-12 p-0">Email</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="email" placeholder="<?php echo $user[0]['email'];?>"
+                                                class="form-control p-0 border-0" name="example-email"
+                                                id="example-email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="col-md-12 p-0">Password</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="password" value="password" class="form-control p-0 border-0">
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="col-md-12 p-0">Phone No</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="text" placeholder="123 456 7890"
+                                                class="form-control p-0 border-0">
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="col-md-12 p-0">Message</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <textarea rows="5" class="form-control p-0 border-0"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="col-sm-12">Select Country</label>
+
+                                        <div class="col-sm-12 border-bottom">
+                                            <select class="form-select shadow-none p-0 border-0 form-control-line">
+                                                <option>London</option>
+                                                <option>India</option>
+                                                <option>Usa</option>
+                                                <option>Canada</option>
+                                                <option>Thailand</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <div class="col-sm-12">
+                                            <button class="btn btn-success">Update Profile</button>
+                                            
+</a>
+                                        </div>
+                                        
+                                    </div>
+                                    <a href="index.php?page=settings&amp;action=delete" style="color:red;">Supprimer mon compte
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <?php $content = ob_get_clean();
+require('template4.php'); ?>
