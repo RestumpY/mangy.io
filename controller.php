14ba38a358080fdf964976d9d34ce19f1316c1f9
MoryKadoch <mory@mory-ThinkPad-X260>
2021-11-25 16:41:43 +0100

    modif mo

--- controller.php
+++ controller.php
@@ -2,38 +2,43 @@
 require 'model.php';
 session_start();
 
-function index(){
+function index()
+{
     require 'views/indexView.php';
 }
 
-function signin(){
-    if($_SESSION['id']) header("Location: index.php?page=dashboard");
-    if($_POST){
+function signin()
+{
+    if ($_SESSION['id']) header("Location: index.php?page=dashboard");
+    if ($_POST) {
         echo addUser($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['password']);
         header("Location: index.php?page=dashboard");
     }
     require 'views/signInView.php';
 }
 
-function login(){
-    if($_SESSION['id']) header("Location: index.php?page=dashboard");
-    if($_POST){
+function login()
+{
+    if ($_SESSION['id']) header("Location: index.php?page=dashboard");
+    if ($_POST) {
         loginUser($_POST['email'], $_POST['password']);
-        if($_SESSION['id']) header("Location: index.php?page=dashboard"); else $alert = 1;
+        if ($_SESSION['id']) header("Location: index.php?page=dashboard");
+        else $alert = 1;
     }
     require 'views/loginView.php';
 }
 
-function dashboard(){
-    if(!$_SESSION['id']) header("Location: index.php?page=signIn");
+function dashboard()
+{
+    if (!$_SESSION['id']) header("Location: index.php?page=signIn");
     $user = getLine('user', 'id', $_SESSION['id']);
     $meteo = meteo();
-    if($_POST['title'] and $_POST['content']){
+    if ($_POST['title'] and $_POST['content']) {
         //print_r($_POST);
         addPost($_SESSION['id'], $_POST['title'], $_POST['content']);
         //if($_SESSION['id']) header("Location: index.php?page=dashboard"); else $alert = 1;
     }
-    if($_POST['idPost'] and $_POST['comment']){
+    if ($_POST['idPost'] and $_POST['comment']) {
         addComment($_SESSION['id'], $_POST['idPost'], $_POST['comment']);
     }
     $posts = getLines('post', '', '');
@@ -41,23 +46,24 @@ function dashboard(){
     require 'views/dashboardView.php';
 }
 
-function dashboard2(){
-    
+function dashboard2()
+{
+
     require 'views/dashboardView.php';
 }
 
 
-function settings(){
-    if(!$_SESSION['id']) header("Location: index.php?page=signIn");
-    if($_POST){
-        foreach($_POST as $key => $value)
-        {
-            if($value!='' and $key!='password') update('user', $key, $value, 'id', $_SESSION['id']);
-            if($value!='' and $key=='password') update('user', $key, md5($value), 'id', $_SESSION['id']);
+function settings()
+{
+    if (!$_SESSION['id']) header("Location: index.php?page=signIn");
+    if ($_POST) {
+        foreach ($_POST as $key => $value) {
+            if ($value != '' and $key != 'password') update('user', $key, $value, 'id', $_SESSION['id']);
+            if ($value != '' and $key == 'password') update('user', $key, md5($value), 'id', $_SESSION['id']);
         }
     }
     $user = getLine('user', 'id', $_SESSION['id']);
-    if($_GET['action']=='delete'){
+    if ($_GET['action'] == 'delete') {
         delete('user', 'id', $_SESSION['id']);
         unset($_SESSION['id']);
         header("Location: index.php?page=signIn");
@@ -73,7 +79,13 @@ function logout()
     exit();
 }
 
-function messagerie(){
-    
+function messagerie()
+{
     require 'views/messagerieView.php';
+}
+
+function profil()
+{
+    $user = getLine('user', 'id', $_SESSION['id']);
+    require 'views/profilView.php';
 }
