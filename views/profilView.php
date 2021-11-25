87534ff6dfd550070187c717e83d4983a5cf3b7c
RestumpY <70935893+RestumpY@users.noreply.github.com>
2021-11-25 15:20:48 +0100

    Interface utilisateur

--- views/profilView.php
+++ views/profilView.php
@@ -31,6 +31,7 @@
                                 </div>
                             </div>-->
                         </div>
+                        azeaeaza
                     </div>
                     <!-- Column -->
                     <!-- Column -->
